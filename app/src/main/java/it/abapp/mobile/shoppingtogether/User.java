4cdc7b14233de1ff0cdc8910cc7a49c7bc2639ac
Alessandro <alebiker@hotmail.it>
2015-05-27 12:14:58 +0200

    ocr branch

--- app/src/main/java/it/abapp/mobile/shoppingtogether/User.java
+++ app/src/main/java/it/abapp/mobile/shoppingtogether/User.java
@@ -1,8 +1,11 @@
 package it.abapp.mobile.shoppingtogether;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+
 
 /**
  * Created by Alessandro on 13/04/2015.
@@ -10,6 +13,7 @@ import java.util.HashMap;
 public class User implements Serializable{
     public String name;
     public float amount = 0;  // personal amount (common not included)
+    public TessBaseAPI api;
 
     public User(String name) {
         this.name = name;
